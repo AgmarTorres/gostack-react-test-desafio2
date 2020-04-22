fbde1ec0a7781e0867ea0853946a38019f8d5db4
Agmar Torres <agmartorres@gmail.com>
2020-04-22 08:01:42 -0300

    Application with concludes test react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from "react";
-
+import React, { useEffect, useState} from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+    api.get('repositories').then( titles =>{
+      setRepositories(titles.data)
+    })
+ 
+  },[])
+
   async function handleAddRepository() {
-    // TODO
-  }
+    const response = await api.post('repositories', {title: "teste", url: "github", techs:['RodeJS', 'React'] })
+      setRepositories([...repositories, response.data])
+    }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete('repositories/'+ id)
+    const repo = repositories.filter( repository => repository.id !== id)
+    setRepositories(repo)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map( repository => (<li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
